76c0b2c47f45e821c443e174026b69cb43cd11c4
Kent Hessenauer <kehessenauer@gmail.com>
2023-12-23 19:36:18 +0100

    added behaviour variations for sheep/goats

--- Infoprojekt/Assets/Scripts/Entities/Npc/Friendly/SheepController.cs
+++ Infoprojekt/Assets/Scripts/Entities/Npc/Friendly/SheepController.cs
@@ -5,6 +5,7 @@ namespace Entities.Npc.Friendly
 {
     public class SheepController : Npc
     {
+        // will use animations in animation import, not in animations folder. 
         public float maxTargetDistance;
         public float minTargetDistance;
 
@@ -14,7 +15,7 @@ namespace Entities.Npc.Friendly
         private NavMeshAgent _agent;
         private Animator _animator;
 
-        // Animations: walk_forward, walk_backwards, run_forward, turn_90_L, turn_90_R, trot_forward, sit_to_stand, stand_to_sit, idle
+        // Animations: walk_forward, (walk_backwards), run_forward, turn_90_L, turn_90_R, trot_forward, sit_to_stand, stand_to_sit, idle
         private string _currentAnimation;
         private Vector3 _destination;
         private float _lastActionTime;
@@ -24,12 +25,13 @@ namespace Entities.Npc.Friendly
         {
             _agent = GetComponent<NavMeshAgent>();
             _animator = GetComponent<Animator>();
-            InvokeRepeating(nameof(StartWalking), 0, cooldown);
+            InvokeRepeating(nameof(RandomAction), 0, cooldown);
         }
 
         private void Update()
         {
-            if (_agent.destination == transform.position && _currentAnimation != "idle") SetAnimation("idle");
+            if (_agent.destination == transform.position && _currentAnimation != "stand_to_sit" &&
+                _currentAnimation != "sit_to_stand") SetAnimation("idle");
         }
 
         private static void OnDeath()
@@ -37,8 +39,17 @@ namespace Entities.Npc.Friendly
             // TODO: death animation, drop items
         }
 
+        private void SetAnimation(string animationName)
+        {
+            if (_currentAnimation == animationName) return;
+            _animator.Play(animationName);
+            _currentAnimation = animationName;
+            Debug.Log("Set animation to: " + animationName);
+        }
+
         // rotate to face the target location, then walk to it
-        private void WalkToRandomLocation()
+        // needs to be changed in order to use different animations (
+        private void WalkToRandomLocation(string animationName = "walk_forward")
         {
             _destination = RandomNavmeshLocation(maxTargetDistance, minTargetDistance);
             var targetRotation = Quaternion.LookRotation(_destination - transform.position);
@@ -50,20 +61,42 @@ namespace Entities.Npc.Friendly
             Invoke(nameof(StartWalking), 1);
         }
 
-        private void StartWalking()
+        private void StartWalking(string animationName = "walk_forward")
         {
             // remove when using WalkToRandomLocation()!!
-            _destination = RandomNavmeshLocation(maxTargetDistance);
+            _destination = RandomNavmeshLocation(maxTargetDistance, minTargetDistance);
 
             _agent.SetDestination(_destination);
-            SetAnimation("walk_forward");
+            SetAnimation(animationName);
         }
 
-        private void SetAnimation(string animationName)
+        private void RandomAction()
         {
-            if (_currentAnimation == animationName) return;
-            _animator.Play(animationName);
-            _currentAnimation = animationName;
+            if (_currentAnimation == "stand_to_sit")
+            {
+                SetAnimation("sit_to_stand");
+                return;
+            }
+
+            var random = Random.Range(0, 5);
+            switch (random)
+            {
+                case 0:
+                    SetAnimation("stand_to_sit");
+                    return;
+                case 1:
+                    StartWalking();
+                    return;
+                case 2:
+                    StartWalking("trot_forward");
+                    return;
+                case 3:
+                    StartWalking("run_forward");
+                    return;
+                case 4:
+                    SetAnimation("eating");
+                    return;
+            }
         }
     }
 }
