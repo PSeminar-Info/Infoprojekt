fac3960deec190c14bfeed50e504761ae2d1953a
KillerRabbit154 <simon.kopp@gmx.net>
2024-05-13 18:46:54 +0200

    Story2

--- Infoprojekt/Assets/Scenes/StoryMaps/Story1/ActivateoldSc.cs
+++ Infoprojekt/Assets/Scenes/StoryMaps/Story1/ActivateoldSc.cs
@@ -8,7 +8,7 @@ public class ActivateoldSc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("LazyLake");
+        SceneManager.LoadScene("AfterLazyLake");
 
     }
 
