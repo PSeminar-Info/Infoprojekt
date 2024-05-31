f4ff78b1cde35a5d62543491dd7385d71902ad68
LL607-sys <LeonhardLang07@outlook.de>
2024-05-31 19:11:46 +0200

    Added first story act talking villagers

--- Infoprojekt/Assets/Scripts/Entities/Npc/Friendly/Villager/TriggerText.cs
+++ Infoprojekt/Assets/Scripts/Entities/Npc/Friendly/Villager/TriggerText.cs
@@ -6,29 +6,31 @@ using UnityEngine.UI;
 public class TriggerText : MonoBehaviour
 {
     public Text text;
-    bool enter = false;
+
+    public string[] stringArray = { "Ich habe keine Zeit für deine Anliegen.", "Geh weg! Ich will nicht mit dir reden.", "Lass mich in Ruhe. Ich habe wichtigere Dinge zu tun.", "Deine Anwesenheit ist hier nicht erwünscht.", "Ich möchte keine Geschäfte mit dir machen.", "Du störst mich. Verschwinde!", "Ich habe keine Lust, mich mit dir abzugeben.", "Ich möchte allein sein. Geh bitte.", "Verschwinde, bevor ich die Wachen rufe.", "Ich will nichts mit Fremden zu tun haben." };
+    private string randomString;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        print("test");
-        enter = true;
+        if (other.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+            System.Random random = new System.Random();
+
+            // Wähle einen zufälligen Index aus dem Array
+            int randomIndex = random.Next(stringArray.Length);
+
+            // Hole den String an diesem zufälligen Index
+            randomString = stringArray[randomIndex];
+
+            text.text = randomString;
+        };
     }
 
     private void OnTriggerExit(Collider other)
     {
-        enter = false;
+        if (other.CompareTag("Player")) text.gameObject.SetActive(false);
     }
 
-    void Update()
-    {
-        if (enter)
-        {
-            text.text = "test";
-           // if (this.tag.Equals("HausUnten"))
-            //{
-                //text.gameObject.SetActive(true);
-                //text.text = "Pff ... mit dir will ich gar nichts zu tun haben";
-            //}
-        }
-    }
 }
